0e9748ef9a7cd807972993707d5d1e6822429267
Broketoof25 <andrew.s.lall@gmail.com>
2024-02-10 09:23:43 -0500

    refactor with promises

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert').strict;
+//don't need asserts anymore, was just using for basic error handling while demonstrating
+//callback hell.  The returned PROMISE will either RESOLVE or REJECT
+
+//const assert = require('assert').strict;
+
 //require the operations module.  now we have access to the 4 CRUD methods in the 
 //operation.js file (module)
 const dboper = require('./operations');
@@ -10,12 +14,14 @@ const url = 'mongodb://localhost:27017/';
 //name of DB we want to connect to
 const dbname = 'nucampsite';
 
-//now to connect we use the Mongo.Client.connect(), the 3rd param the function callback
+//now to connect we use the Mongo.Client.connect()
 //gives us access to 'client' object that we can use to interact with DB
-MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
-    //assert params err is what we are checking, null is the expected value
-    //we are checking against to see if first arg equals second
-    assert.strictEqual(err, null);
+//the below method returns a PROMISE so we can chain with a .then, if RESOLVE will return
+//'client' as resolved value.  The PROMISE .then method takes a callback function as it's
+//parameter
+//the MongoClient method below returns a PROMISE, if it fails it gets caught in the CATCH at the bottom  
+MongoClient.connect(url, {useUnifiedTopology: true}).then(client => {
+
     console.log('Connected correctly to server');
 
     //interact with server through DB interactions
@@ -27,38 +33,54 @@ MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
     //notice how the code is nested with a series of callback functions
     //this is because it's asynchronous since we are connecting to a DB
     //this is also callback hell and can be fixed with promise chaining
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null);
-        console.log('Dropped Collection', result)
+    db.dropCollection('campsites')
+    .then(result => {
+        console.log('Dropped Collection', result);
+    })
+    .catch(err => {
+        console.log('No collection to drop.')
+    })
 
-        //insert a document into the Collection
-        dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites', result => {
-            console.log('Insert Document:', result.ops);
+    //insert a document into the Collection
+    //insertDocument handles below handles each of the DB operations below in CHAIN, waiting for the first to 
+    //resolve, then continuing on to next.
+    dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites') 
+    //since we are using .then to chain, the RESULT variable below will hold the value from the PROMISE
+    .then (result => {
+        console.log('Insert Document:', result.ops);
 
-            dboper.findDocuments(db, 'campsites', docs => {
-                console.log('Found Documents:', docs);
+            return dboper.findDocuments(db, 'campsites');
+    })
+    .then(docs => {
+        console.log('Found Documents:', docs);
+    
                 
                 //in the params list - look for a Document with name: Breadcrumb Trail
                 // Campground and update the description field with the value below
-                dboper.updateDocument(db, {name: "Breadcrumb Trail Campground"}, {description: "Updated Test Description"}, 'campsites', result => {
-                    console.log("Updated Document Count: ", result.result.nModified);
+        return dboper.updateDocument(db, {name: "Breadcrumb Trail Campground"}, {description: "Updated Test Description"}, 'campsites');
+    })
+    .then(result => {
+        console.log("Updated Document Count: ", result.result.nModified);
 
-                    //call findDocuments again and console.log to confirm update has 
-                    //occured
-                    dboper.findDocuments(db, 'campsites', docs => {
-                        console.log("Found Documents: ", docs);
+        //call findDocuments again and console.log to confirm update has 
+        //occured
+        return dboper.findDocuments(db, 'campsites'); 
+    })
+    .then (docs => {
+        console.log("Found Documents: ", docs);
 
-                        dboper.removeDocument(db, {name: "Breadcrumb Trail Campground"}, 'campsites', result => {
-                            console.log('Deleted Document Count: ', result.deletedCount)
+        return dboper.removeDocument(db, {name: "Breadcrumb Trail Campground"}, 'campsites');
+    })
+    .then (result => {
+        console.log('Deleted Document Count: ', result.deletedCount)
 
-                            //close connection
-                            client.close();
-                        }   
-                    );
-                });
-            }    
-        );
+        //close connection
+        return client.close();
+    })
+    .catch(err => {
+        console.log(err);
+        client.close();
     });
-});
-});
-});
+})
+//we can chain the catch method here
+.catch(err => console.log(err));
--- operations.js
+++ operations.js
@@ -1,47 +1,42 @@
 //we will write 4 methods to insert, find, remove and update documents
 //we will be using the assert module, strict version so we will require it
-const assert = require('assert').strict;
+//we can remove the require for assert since we are not using it anymore
+//and moved to returning PROMISES, the PROMISE will return a RESOLVE OR REJECT
+
+//const assert = require('assert').strict;
 
 //insert - 4 params for this method
 //insertDocument has access to collection in connected DB, so can now use 
 //insertOne() method of the collection
-exports.insertDocument = (db, document, collection, callback) => {
+//since no callback in the param list, this method will return a PROMISE
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
     //find() with an empty param list means we want to find all documents in Collection
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    })
+    //since no callback in the param list, this method will return a PROMISE
+    return coll.find().toArray()
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
     //the document arg is the JS object passed in that will be used to figure out
     //which document to delete.
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    })
+    //since no callback in the param list, this method will return a PROMISE
+    return coll.deleteOne(document)
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
     //updateOne() has 4 params, document - info on object we want to update
     //second param is an object with the upates we want to make, we will use the $set
     //mongoDB will see that and know we want to overwrite with 'update'
     //third param to pass in optional configs
     //4th is a callback function that will give us an error or the result of the operation
-    coll.updateOne(document, {$set: update}, null, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    })
+    //since no callback in the param list, this method will return a PROMISE
+    return coll.updateOne(document, {$set: update}, null)
 };
 
